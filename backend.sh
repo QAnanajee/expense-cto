16940ab25f2162abfe044c56cec4160d5a9a33ae
Nanajee ad <nanajee.k9@gmail.com>
2024-05-12 10:59:18 +0530

    add cto files

--- backend.sh
+++ backend.sh
@@ -41,7 +41,7 @@ unzip /tmp/backend.zip &>>LOGFILE
 npm install &>>LOGFILE
 #VALIDATE $? "Installing nodejs dependencies"
 
-cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service &>>LOGFILE
+cp /home/ec2-user/expense-cto/backend.service /etc/systemd/system/backend.service &>>LOGFILE
 #VALIDATE $? "Copied backend service"
 
 systemctl daemon-reload &>>LOGFILE
